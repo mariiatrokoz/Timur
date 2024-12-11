f807c6f7091d839125ab3bc8b3bafb5a58434ec1
TimiTeam <timatiblackstar666@gmail.com>
2024-12-11 23:01:21 +0200

    to maria

---
+++ to_maria.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clean='\033[0m'
+green='\033[0;32m'
+red='\033[0;31m'
+cyan='\033[0;36m'  
+purple='\033[0;35m'
+
+echo "\n\t${purple}Hello ${red}I love${green} you ${cyan}tell ${red}me ${purple}your ${green}name${clean}\n"
+
