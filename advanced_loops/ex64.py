a8fc521719d364674d696207ac08c5aaf88bc085
TimiTeam <timatiblackstar666@gmail.com>
2025-01-06 23:06:02 +0200

    upd task

--- advanced_loops/ex64.py
+++ advanced_loops/ex64.py
@@ -10,11 +10,32 @@
 Переконайтеся, що розміри знижок і нові ціни округлені до 2 десяткових знаків,
 коли вони відображаються.'''
 
+
+def print_prices_with_discount(price_tablet):
+    
+    for price in price_tablet:
+        print(f"{price[0]}\t{round(price[1], 2)}\t{round(price[2], 2)}")
+
+
+def create_price_table(prices, discount):
+    price_table = []
+    
+    #...
+    #...
+    
+    return price_table
+
+
 prices = [4.95, 9.95, 14.95, 19.95, 24.95]
 
-for price in prices:
-    discount = round(price * 0.6, 2)
-    print(f"{price}\t{round(price - discount, 2)}\t{discount}")
+#for price in prices:
+#    discount = round(price * 0.6, 2)
+#    print(f"{price}\t{round(price - discount, 2)}\t{discount}")
+
+price_table = create_price_table(prices, 0.6)
+
+print_prices_with_discount(price_table)
+
 
 
 
@@ -23,6 +44,5 @@ key     price   discount_size
 apple   4.96,   3.12
 water   9.95,   4.30
 ...
-
 
 '''
