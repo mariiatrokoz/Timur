bc92ecf27ddb4ec6bb4306ab3c3f0fdf64f87e1f
Maria <mariiatrokoz@gmail.com>
2025-01-22 16:43:21 +0200

    simple .sh script

---
+++ conditional_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "do you miss me?"
+echo "enter 'y' for yes and 'n' for no"
+read response
+if [ "$response" = "y" ]; then
+echo "I miss u 2!:***"
+else
+    echo "Oh, okay. That's fine!"
+fi
